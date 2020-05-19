95419851cbc4821dc69e22598ce7197a47b80347
amgillenwater <amgillenwater@gmail.com>
2020-05-19 19:46:38 -0400

    function from pair coding to remove stop words

---
+++ class_code.py
@@ -0,0 +1,5 @@
+# 1. Write a function `remove_from_list(list_of_items, item_to_remove)` that takes a list of items and an item to remove from that list and returns a new list with the item removed. Write code to test this.
+# 2. Make sure your function removes all instances of the item. Write code to test this. `remove_from_list(['MI', 'AK', 'SC', 'AK', 'DE'], 'AK')` should return `['MI', 'SC', 'DE']`.
+# 3. Write a function `remove_many_from_list(list_of_items, items_to_remove)` that takes two lists, a list of items, and a list of items to remove from that original list. You can re-use `remove_from_list` or write it from scratch.
+
+def remove_from_list(list_of_items, item_to_remove)
--- word_frequency.py
+++ word_frequency.py
@@ -7,7 +7,7 @@ STOP_WORDS = [
 
 def print_word_freq(file):
     """Read in `file` and print out the frequency of words in that file."""
-    pass
+    
 
 
 if __name__ == "__main__":
@@ -25,3 +25,11 @@ if __name__ == "__main__":
     else:
         print(f"{file} does not exist!")
         exit(1)
+
+
+def remove_from_list(list_of_items, items_to_remove):
+    new_list = []
+    for item in list_of_items:
+        if not item in items_to_remove:
+            new_list.append(item)
+    return(new_list)
